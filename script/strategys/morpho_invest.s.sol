e17f3ed351dc9a0312370f4b26cc76b18e54d8be
shaneson <shaneson@shanesondeMacBook-Pro.local>
2024-09-16 13:56:20 +0800

    add readme

--- script/strategys/morpho_invest.s.sol
+++ script/strategys/morpho_invest.s.sol
@@ -9,7 +9,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {InfiniCardVault} from "../../src/InfiniCardVault.sol";
 
 contract MorphoInvestScript is Script {
-    // forge script script/strategys/morpho_invest.s.sol:MorphoInvestScript --rpc-url https://eth.llamarpc.com --broadcast --legacy
+    // forge script script/strategys/morpho_invest.s.sol:MorphoInvestScript --rpc-url https://eth-pokt.nodies.app --broadcast --legacy
     function run() external {
         // 1. send usdc to infiniCardVault
 
@@ -19,13 +19,16 @@ contract MorphoInvestScript is Script {
         vm.startBroadcast(adminPrivateKey);
 
         address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        address payable infiniCardVault = payable(0x09E52AA36484c20288D9C148458Ea4DA991118Af);
-        address morpho = 0x6ac25F85a8fA9a7D77B3f2165103Fc0F09642B6A;
 
-        uint256 usdcBalance = IERC20(USDC).balanceOf(adminRole);
-        IERC20(USDC).transfer(infiniCardVault, usdcBalance);
+        // uint256 usdcBalance = IERC20(USDC).balanceOf(adminRole);
+        // IERC20(USDC).transfer(infiniCardVault, usdcBalance);
 
         // 2. deposit usdc to morpho
+        address vault = 0x09E52AA36484c20288D9C148458Ea4DA991118Af;
+        address morpho = 0x6ac25F85a8fA9a7D77B3f2165103Fc0F09642B6A;
+        address payable infiniCardVault = payable(0x09E52AA36484c20288D9C148458Ea4DA991118Af);
+
+        uint256 usdcBalance = IERC20(USDC).balanceOf(vault);
         InfiniCardVault(infiniCardVault).invest(address(morpho), usdcBalance);
 
         vm.stopBroadcast();
